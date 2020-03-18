f6a7d6a1d416b87c06d7fcbd337cf851e90b5453
Dmitriy Finozhenok <dmitry.finozhenok@aspose.com>
2020-03-18 15:50:28 +0900

    Split tests to domain related groups

---
+++ tests/base_test.go
@@ -0,0 +1,206 @@
+//
+// MIT License
+
+// Copyright (c) 2019 Aspose Pty Ltd
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+package api_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/aspose-words-cloud/aspose-words-cloud-go/api"
+	"github.com/aspose-words-cloud/aspose-words-cloud-go/api/models"
+)
+
+var remoteBaseTestDataFolder string = "Temp/SdkTests/TestData"
+
+var commonTestFile string = GetLocalPath("Common", "test_multi_pages.docx")
+
+func GetLocalPath(folderPath string, filename string) string {
+	_, currentFilePath, _, _ := runtime.Caller(0)
+	currentFolder := filepath.Dir(currentFilePath)
+	return filepath.Join(currentFolder, "testdata", folderPath, filename)
+}
+
+func ReadConfiguration(t *testing.T) (cfg *models.Configuration) {
+	_, filename, _, _ := runtime.Caller(0)
+	configFile := filepath.Join(filepath.Dir(filename), "../config.json")
+	configuration, err := models.NewConfiguration(configFile)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	return configuration
+}
+
+func PrepareTest(t *testing.T, config *models.Configuration) (apiClient *api.APIClient, ctx context.Context) {
+	client, err := api.NewAPIClient(config)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	context, err := client.NewContextWithToken(nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	return client, context
+}
+
+func UploadFileToStorage(t *testing.T, fileName string, path string) (*api.APIClient, context.Context) {
+	config := ReadConfiguration(t)
+	client, ctx := PrepareTest(t, config)
+
+	UploadNextFileToStorage(t, ctx, client, fileName, path)
+
+	return client, ctx
+}
+
+func UploadNextFileToStorage(t *testing.T, ctx context.Context, client *api.APIClient, fileName string, path string) {
+	file, fileErr := os.Open(fileName)
+	if fileErr != nil {
+		t.Error(fileErr)
+	}
+
+	_, _, err := client.WordsApi.UploadFile(ctx, file, path, nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDebugMode(t *testing.T) {
+	config := ReadConfiguration(t)
+	config.DebugMode = true
+	client, ctx := PrepareTest(t, config)
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+
+	_, _, err := client.WordsApi.CreateDocument(ctx, map[string]interface{}{
+		"fileName": "test.docx",
+	})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	logData := buf.String()
+
+	url := "PUT /v4.0/words/create?FileName=test.docx"
+	success := " 200 OK"
+
+	if !strings.Contains(logData, url) {
+		t.Errorf("Output doesn't contain the string '%s'", url)
+	}
+
+	if !strings.Contains(logData, success) {
+		t.Errorf("Output doesn't contain the string '%s'", success)
+	}
+}
+
+func TestCoverage(t *testing.T) {
+	_, currentFileName, _, _ := runtime.Caller(0)
+	currentFolder := filepath.Dir(currentFileName)
+	apiFileName := filepath.Join(currentFolder, "../api/words_api.go")
+	api, err := ioutil.ReadFile(apiFileName)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	declareFuncRegex := regexp.MustCompile(`func \(a \*WordsApiService\) (\w+)`)
+	allApiFuncNames := declareFuncRegex.FindAllSubmatch(api, -1)
+
+	var exists = struct{}{}
+
+	notTestedApiFuncNames := make(map[string]struct{})
+
+	for _, name := range allApiFuncNames {
+		notTestedApiFuncNames[string(name[1])] = exists
+	}
+
+	testFileNames, _ := filepath.Glob(filepath.Join(currentFolder, "*.go"))
+
+	for _, testFileName := range testFileNames {
+		tests, err := ioutil.ReadFile(testFileName)
+		if err != nil {
+			t.Error(err)
+		}
+
+		callFuncRegex := regexp.MustCompile(`\.WordsApi\.(\w+)`)
+		testedlApiFuncNames := callFuncRegex.FindAllSubmatch(tests, -1)
+
+		for _, name := range testedlApiFuncNames {
+			delete(notTestedApiFuncNames, string(name[1]))
+		}
+	}
+
+	if len(notTestedApiFuncNames) > 0 {
+		report := "Not covered methods:\n"
+
+		for name := range notTestedApiFuncNames {
+			report = report + fmt.Sprintf("\t%s\n", name)
+		}
+
+		t.Error(report)
+	}
+}
+
+func TestGetAvailableFonts(t *testing.T) {
+
+	config := ReadConfiguration(t)
+	client, ctx := PrepareTest(t, config)
+
+	_, _, err := client.WordsApi.GetAvailableFonts(ctx, nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestResetCache(t *testing.T) {
+
+	config := ReadConfiguration(t)
+	client, ctx := PrepareTest(t, config)
+
+	_, err := client.WordsApi.ResetCache(ctx)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
---
+++ tests/mailMerge_test.go
@@ -0,0 +1,84 @@
+//
+// MIT License
+
+// Copyright (c) 2019 Aspose Pty Ltd
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+package api_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+)
+
+func TestExecuteMailMerge(t *testing.T) {
+
+	localFolder := filepath.Join("DocumentActions", "MailMerge")
+	localFilePath := GetLocalPath(localFolder, "SampleMailMergeTemplate.docx")
+	remoteFolder := path.Join(remoteBaseTestDataFolder, "DocumentActions", "MailMerge")
+	remoteName := "TestExecuteMailMerge.docx"
+	data, fileErr := ioutil.ReadFile(GetLocalPath(localFolder, "SampleMailMergeTemplateData.txt"))
+	if fileErr != nil {
+		t.Error(fileErr)
+	}
+	options := map[string]interface{}{
+		"data":        string(data),
+		"folder":      remoteFolder,
+		"withRegions": false,
+	}
+
+	client, ctx := UploadFileToStorage(t, localFilePath, path.Join(remoteFolder, remoteName))
+
+	_, _, err := client.WordsApi.ExecuteMailMerge(ctx, remoteName, options)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestExecuteMailMergeOnline(t *testing.T) {
+
+	localFolder := filepath.Join("DocumentActions", "MailMerge")
+	localTemplatePath := GetLocalPath(localFolder, "SampleMailMergeTemplate.docx")
+	localDataPath := GetLocalPath(localFolder, "SampleMailMergeTemplateData.txt")
+
+	template, err := os.Open(localTemplatePath)
+	if err != nil {
+		t.Error(err)
+	}
+	data, err := os.Open(localDataPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	config := ReadConfiguration(t)
+	client, ctx := PrepareTest(t, config)
+
+	output, err := client.WordsApi.ExecuteMailMergeOnline(ctx, template, data, nil)
+	defer output.Body.Close()
+
+	if err != nil {
+		t.Error(err)
+	}
+}
