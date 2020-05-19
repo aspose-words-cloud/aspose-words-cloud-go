3cc589cec48e99b3b8cc8d4000879c0efd53fc9f
Dmitriy Finozhenok <dmitry.finozhenok@aspose.com>
2020-05-19 19:57:57 +0900

    Add hyper-v isolation

--- Scripts/RunTestsInDocker.bat
+++ Scripts/RunTestsInDocker.bat
@@ -1,3 +1,3 @@
 for /f %%i in ('dir v* /b/a:d/od/t:c') do set LAST=%%i
 echo {"AppSid":"%WordsAppSid%","AppKey":"%WordsAppKey%","BaseUrl":"%apiUrl%" } > %LAST%/config.json
-docker run -v %cd%:c:/sdk -w="c:/sdk/" --rm -t golang:1.14.0-windowsservercore-1809 c:/sdk/Scripts/RunTestsStandalone.bat
+docker run --isolation=hyperv -v %cd%:c:/sdk -w="c:/sdk/" --rm -t golang:1.14.0-windowsservercore-1809 c:/sdk/Scripts/RunTestsStandalone.bat
