6a12713809e6effbe367f6147815c3c276b7c930
evgeny.ivanov <evgeny.ivanov>
2020-11-10 22:55:50 -0600

    SDK regenerated by CI server [ci skip]

--- dev/api/api_client.go
+++ dev/api/api_client.go
@@ -59,7 +59,7 @@ var (
     xmlCheck = regexp.MustCompile("(?i:[application|text]/xml)")
 )
 
-// APIClient manages communication with the Aspose.Words for Cloud API Reference API v20.10
+// APIClient manages communication with the Aspose.Words for Cloud API Reference API v20.11
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
     cfg 	*models.Configuration
