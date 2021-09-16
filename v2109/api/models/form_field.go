1a91d87734419209ac9c2ae4d56df38ed3f95ff7
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2021-09-16 14:40:26 +0900

    prepare v2109 release

---
+++ v2109/api/models/form_field.go
@@ -0,0 +1,112 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="form_field.go">
+ *   Copyright (c) 2021 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+package models
+
+// FromField.
+type FormFieldResult struct {
+    // FromField.
+    Link WordsApiLinkResult `json:"Link,omitempty"`
+
+    // FromField.
+    NodeId string `json:"NodeId,omitempty"`
+
+    // FromField.
+    CalculateOnExit bool `json:"CalculateOnExit,omitempty"`
+
+    // FromField.
+    Enabled bool `json:"Enabled,omitempty"`
+
+    // FromField.
+    EntryMacro string `json:"EntryMacro,omitempty"`
+
+    // FromField.
+    ExitMacro string `json:"ExitMacro,omitempty"`
+
+    // FromField.
+    HelpText string `json:"HelpText,omitempty"`
+
+    // FromField.
+    Name string `json:"Name,omitempty"`
+
+    // FromField.
+    OwnHelp bool `json:"OwnHelp,omitempty"`
+
+    // FromField.
+    OwnStatus bool `json:"OwnStatus,omitempty"`
+
+    // FromField.
+    StatusText string `json:"StatusText,omitempty"`
+}
+
+type FormField struct {
+    // FromField.
+    Link IWordsApiLink `json:"Link,omitempty"`
+
+    // FromField.
+    NodeId *string `json:"NodeId,omitempty"`
+
+    // FromField.
+    CalculateOnExit *bool `json:"CalculateOnExit,omitempty"`
+
+    // FromField.
+    Enabled *bool `json:"Enabled,omitempty"`
+
+    // FromField.
+    EntryMacro *string `json:"EntryMacro,omitempty"`
+
+    // FromField.
+    ExitMacro *string `json:"ExitMacro,omitempty"`
+
+    // FromField.
+    HelpText *string `json:"HelpText,omitempty"`
+
+    // FromField.
+    Name *string `json:"Name,omitempty"`
+
+    // FromField.
+    OwnHelp *bool `json:"OwnHelp,omitempty"`
+
+    // FromField.
+    OwnStatus *bool `json:"OwnStatus,omitempty"`
+
+    // FromField.
+    StatusText *string `json:"StatusText,omitempty"`
+}
+
+type IFormField interface {
+    IsFormField() bool
+}
+func (FormField) IsFormField() bool {
+    return true
+}
+
+func (FormField) IsNodeLink() bool {
+    return true
+}
+
+
---
+++ v2109/api/models/search_online_request.go
@@ -0,0 +1,129 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="search_online_request.go">
+ *   Copyright (c) 2021 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+package models
+
+import (
+	"io/ioutil"
+	"net/url"
+	"strings"
+    "io"
+    "encoding/json"
+)
+
+// SearchOnlineRequest contains request data for WordsApiService.SearchOnline method.
+type SearchOnlineRequest struct {
+        // The document.
+        Document io.ReadCloser
+        // The regular expression used to find matches.
+        Pattern *string
+    /* optional (nil or map[string]interface{}) with one or more of key / value pairs:
+        key: "loadEncoding" value: (*string) Encoding that will be used to load an HTML (or TXT) document if the encoding is not specified in HTML.
+        key: "password" value: (*string) Password for opening an encrypted document. */
+    Optionals map[string]interface{}
+}
+
+
+func (data *SearchOnlineRequest) CreateRequestData() (RequestData, error) {
+
+    var result RequestData
+
+    result.Method = strings.ToUpper("put")
+
+    // create path and map variables
+    result.Path = "/words/online/get/search"
+
+    result.Path = strings.Replace(result.Path, "/<nil>", "", -1)
+    result.Path = strings.Replace(result.Path, "//", "/", -1)
+
+    result.HeaderParams = make(map[string]string)
+    result.QueryParams = url.Values{}
+    result.FormParams = make([]FormParamContainer, 0)
+
+    if err := typeCheckParameter(data.Optionals["loadEncoding"], "string", "data.Optionals[loadEncoding]"); err != nil {
+        return result, err
+    }
+    if err := typeCheckParameter(data.Optionals["password"], "string", "data.Optionals[password]"); err != nil {
+        return result, err
+    }
+
+
+    result.QueryParams.Add("Pattern", parameterToString(*data.Pattern, ""))
+
+
+    if localVarTempParam, localVarOk := data.Optionals["loadEncoding"].(string); localVarOk {
+        result.QueryParams.Add("LoadEncoding", parameterToString(localVarTempParam, ""))
+    }
+
+
+    if localVarTempParam, localVarOk := data.Optionals["password"].(string); localVarOk {
+        result.QueryParams.Add("Password", parameterToString(localVarTempParam, ""))
+    }
+
+
+    // to determine the Content-Type header
+    localVarHttpContentTypes := []string{ "multipart/form-data", }
+
+    // set Content-Type header
+    localVarHttpContentType := selectHeaderContentType(localVarHttpContentTypes)
+    if localVarHttpContentType != "" {
+        result.HeaderParams["Content-Type"] = localVarHttpContentType
+    }
+
+    // to determine the Accept header
+    localVarHttpHeaderAccepts := []string{
+        "application/xml",
+        "application/json",
+    }
+
+    // set Accept header
+    localVarHttpHeaderAccept := selectHeaderAccept(localVarHttpHeaderAccepts)
+    if localVarHttpHeaderAccept != "" {
+        result.HeaderParams["Accept"] = localVarHttpHeaderAccept
+    }
+
+
+    _document := data.Document
+    if _document != nil {
+        fbs, _ := ioutil.ReadAll(_document)
+        _document.Close()
+        result.FormParams = append(result.FormParams, NewFileFormParamContainer("document", fbs))
+    }
+
+
+
+    return result, nil
+}
+
+func (data *SearchOnlineRequest) CreateResponse(reader io.Reader, boundary string) (response interface{}, err error) {
+            var successPayload SearchResponse
+            if err = json.NewDecoder(reader).Decode(&successPayload); err != nil {
+                return nil, err
+            }
+
+            return successPayload, err
+}
