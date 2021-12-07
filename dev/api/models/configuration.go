fbb425ca14d9d0563c8bb731ac2e92e2b5da86fb
jenkins <jenkins.aspose@gmail.com>
2021-12-07 00:22:52 -0600

    SDK regenerated by CI server [ci skip]

--- dev/api/models/configuration.go
+++ dev/api/models/configuration.go
@@ -94,7 +94,7 @@ func NewConfiguration(configFilePath string) (pConfig *Configuration, err error)
 	cfg := Configuration{
 		BaseUrl:       "https://api.aspose.cloud",
 		DebugMode:     false,
-		DefaultHeader: map[string]string{"x-aspose-client": "go sdk", "x-aspose-client-version": "21.11"},
+		DefaultHeader: map[string]string{"x-aspose-client": "go sdk", "x-aspose-client-version": "21.12"},
 	}
 	err = json.Unmarshal(data, &cfg)
 
