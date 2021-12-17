667b65a9d59035bdb173314171a4378ac20904cf
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2021-12-17 16:56:39 +0900

    WORDSCLOUD-1860

---
+++ Scripts/RunHealthCheckTestsInDocker.bat
@@ -0,0 +1,2 @@
+echo {"ClientId":"%ClientId%","ClientSecret":"%ClientSecret%","BaseUrl":"%apiUrl%" } > config.json
+docker run --isolation=hyperv -v %cd%:c:/sdk -w="c:/sdk/" --rm -t golang:1.14.0-windowsservercore-1809 c:/sdk/Scripts/RunHealthCheckTestsStandalone.bat %1
