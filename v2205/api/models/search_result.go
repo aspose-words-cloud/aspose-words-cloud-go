da179a4bc3131182a46d77e06de2cdf728e8ef91
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2022-05-18 15:57:23 +0900

    Release 22.5

---
+++ v2205/api/models/search_result.go
@@ -0,0 +1,70 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="search_result.go">
+ *   Copyright (c) 2022 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+package models
+
+// Result of search operation.
+type SearchResultResult struct {
+    // Result of search operation.
+    RangeEnd DocumentPositionResult `json:"RangeEnd,omitempty"`
+
+    // Result of search operation.
+    RangeStart DocumentPositionResult `json:"RangeStart,omitempty"`
+}
+
+type SearchResult struct {
+    // Result of search operation.
+    RangeEnd IDocumentPosition `json:"RangeEnd,omitempty"`
+
+    // Result of search operation.
+    RangeStart IDocumentPosition `json:"RangeStart,omitempty"`
+}
+
+type ISearchResult interface {
+    IsSearchResult() bool
+    Initialize()
+}
+
+func (SearchResult) IsSearchResult() bool {
+    return true
+}
+
+
+func (obj *SearchResult) Initialize() {
+    if (obj.RangeEnd != nil) {
+        obj.RangeEnd.Initialize()
+    }
+
+
+
+    if (obj.RangeStart != nil) {
+        obj.RangeStart.Initialize()
+    }
+
+}
+
+
