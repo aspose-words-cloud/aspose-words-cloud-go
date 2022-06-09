246cbe63a98ed5442fd5ef6866acf6004d525b74
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2022-06-09 20:08:37 +0900

    releases 2206

---
+++ v2206/api/models/office_math_object.go
@@ -0,0 +1,113 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="office_math_object.go">
+ *   Copyright (c) 2022 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+package models
+
+// DTO container with an OfficeMath object.
+type OfficeMathObjectResult struct {
+    // DTO container with an OfficeMath object.
+    Link WordsApiLinkResult `json:"Link,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    NodeId string `json:"NodeId,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    Content StoryChildNodesResult `json:"Content,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    DisplayType string `json:"DisplayType,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    Justification string `json:"Justification,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    MathObjectType string `json:"MathObjectType,omitempty"`
+}
+
+type OfficeMathObject struct {
+    // DTO container with an OfficeMath object.
+    Link IWordsApiLink `json:"Link,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    NodeId *string `json:"NodeId,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    Content IStoryChildNodes `json:"Content,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    DisplayType *string `json:"DisplayType,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    Justification *string `json:"Justification,omitempty"`
+
+    // DTO container with an OfficeMath object.
+    MathObjectType *string `json:"MathObjectType,omitempty"`
+}
+
+type IOfficeMathObject interface {
+    IsOfficeMathObject() bool
+    Initialize()
+}
+
+func (OfficeMathObject) IsOfficeMathObject() bool {
+    return true
+}
+
+func (OfficeMathObject) IsOfficeMathLink() bool {
+    return true
+}
+
+func (OfficeMathObject) IsNodeLink() bool {
+    return true
+}
+
+func (OfficeMathObject) IsLinkElement() bool {
+    return true
+}
+
+func (obj *OfficeMathObject) Initialize() {
+    if (obj.Link != nil) {
+        obj.Link.Initialize()
+    }
+
+
+
+
+
+    if (obj.Content != nil) {
+        obj.Content.Initialize()
+    }
+
+
+
+
+
+
+
+}
+
+
---
+++ v2206/api/models/section_link.go
@@ -0,0 +1,73 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="section_link.go">
+ *   Copyright (c) 2022 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+package models
+
+// Section link element.
+type SectionLinkResult struct {
+    // Section link element.
+    Link WordsApiLinkResult `json:"Link,omitempty"`
+
+    // Section link element.
+    NodeId string `json:"NodeId,omitempty"`
+}
+
+type SectionLink struct {
+    // Section link element.
+    Link IWordsApiLink `json:"Link,omitempty"`
+
+    // Section link element.
+    NodeId *string `json:"NodeId,omitempty"`
+}
+
+type ISectionLink interface {
+    IsSectionLink() bool
+    Initialize()
+}
+
+func (SectionLink) IsSectionLink() bool {
+    return true
+}
+
+func (SectionLink) IsNodeLink() bool {
+    return true
+}
+
+func (SectionLink) IsLinkElement() bool {
+    return true
+}
+
+func (obj *SectionLink) Initialize() {
+    if (obj.Link != nil) {
+        obj.Link.Initialize()
+    }
+
+
+
+}
+
+
