16afb05bd7fffdf607948b093e944ca371987522
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2024-02-26 15:16:00 +0900

    prepare to 24.2 release

---
+++ v2402/tests/urlEncode_test.go
@@ -0,0 +1,59 @@
+/*
+ * --------------------------------------------------------------------------------
+ * <copyright company="Aspose" file="urlEncode_test.go">
+ *   Copyright (c) 2024 Aspose.Words for Cloud
+ * </copyright>
+ * <summary>
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * </summary>
+ * --------------------------------------------------------------------------------
+ */
+
+package api_test
+
+import (
+    "path"
+    "testing"
+    "github.com/aspose-words-cloud/aspose-words-cloud-go/v2402/api/models"
+)
+
+func TestUrlEncode(t *testing.T) {
+
+    localFilePath := commonTestFile
+    remoteFolder := path.Join(remoteBaseTestDataFolder, "DocumentElements", "Section")
+    remoteName := "[“Test_Two,_Inc.”]-_83(b)Election([“Bill_Gates”]).docx"
+    sectionIndex := 0
+    options := map[string]interface{}{
+        "folder": remoteFolder,
+    }
+
+    client, ctx := UploadFileToStorage(t, localFilePath, path.Join(remoteFolder, remoteName))
+
+    request := &models.DeleteSectionRequest{
+        Name: ToStringPointer(remoteName),
+        SectionIndex: ToInt32Pointer(int32(sectionIndex)),
+        Optionals: options,
+    }
+    _, err := client.WordsApi.DeleteSection(ctx, request)
+
+    if err != nil {
+        t.Error(err)
+    }
+}
+
